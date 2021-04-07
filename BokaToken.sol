bc983622aecbf5477c6fd7c1bb99b7c8e0233b3d
Nicholas A Hrboka <nicholashrboka@gmail.com>
2021-04-07 18:00:13 +0200

    added bokatoken for testing

---
+++ BokaToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.1;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract BokaToken {
+    string public name = "BokaToken";
+    string public symbol = "BOKA";
+    uint public decimals = 8;
+}
